d76681649d27f438993ddbbc1ce5a8c06b66b7e5
Sachi-CloudAnalogy <sachi.gaur@cloudanalogy.com>
2024-02-16 19:14:39 +0530

    Day3

---
+++ intro.py
@@ -0,0 +1,20 @@
+print("Hello World !!")
+print("First page", "First Code")
+print(455)
+print(34+20)
+print(50/5)
+
+#variables
+name = "Sachi Gaur"
+year = 2024
+price = 999.99
+bool = True
+a = None
+
+print("My name is :", name)
+print("Year is :",year)
+print(type(name))
+print(type(year))
+print(type(price))
+print(type(bool))
+print(type(a))
---
+++ operations.py
@@ -0,0 +1,14 @@
+#Operations
+a = 400
+b = 150
+sum = a+b
+diff = a-b
+product = a*b
+divide = a/b
+modulo = a%b
+
+print(sum)
+print(diff)
+print(product)
+print(divide)
+print(modulo)
